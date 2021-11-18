f3a0aaec2828a8cf3964c09f80f16882d9206534
Denis Dedkov <dephyys@gmail.com>
2021-11-18 23:21:59 +0300

    Add find in tree
    Added two functions:
     - 'tree_find()' simply finds node in the tree.
     - 'tree_find_path()' gives path to node.
    They will be used in akinator to compare or define entries.

--- include/tree.h
+++ include/tree.h
@@ -1,6 +1,8 @@
 #ifndef TREE_H
 #define TREE_H
 
+#include <stack.h>
+
 struct node {
         node *left  = nullptr;
         node *right = nullptr;
@@ -14,6 +16,29 @@ struct node {
  * Note! It calls action() before next jump.
  */
 void visit_tree(node *root, void (*action)(node *nd));
+
+/*
+ * Finds node with 'data' data.
+ * If no such node found nullptr is returned.
+ */
+node *tree_find(node *root, const char *const data);
+
+/*
+ * Finds node with 'data' data.
+ * If no such node found nullptr is returned.
+ *
+ * Fills 'path' stack with addresses of nodes that lie on the path.
+ * Note! Path is in direct order.
+ *
+ *                      A
+ *                    /  \
+ *                   B   E
+ *                 /  \
+ *                C   D
+ *
+ * Path to D node by stack pop(): A->B->D 
+ */
+node *tree_find_path(node *root, const char *const data, stack_t *path);
 
 #define TREE_DEBUG
 
--- lib/tree.cpp
+++ lib/tree.cpp
@@ -1,8 +1,47 @@
 #include <assert.h>
-
 #include <logs.h>
 #include <tree.h>
+#include <string.h>
+#include <stack.h>
 
+node *tree_find_path(node *root, const char *const data, stack_t *path)
+{
+        assert(root);
+        assert(data);
+        assert(path);
+
+        node *found = nullptr;
+
+        if (!strcmp(root->data, data))
+                found = root;
+        if (root->left  && !found)  
+                found = tree_find_path(root->left,  data, path);
+        if (root->right && !found) 
+                found = tree_find_path(root->right, data, path);
+
+        if (found)
+                push_stack(path, root);
+
+        return found;
+}
+
+node *tree_find(node *root, const char *const data)
+{
+        assert(root);
+        assert(data);
+
+        if (!strcmp(root->data, data))
+                return root; 
+
+        node *found = nullptr;
+        if (root->left)  
+                found = tree_find(root->left,  data);
+
+        if (root->right && !found) 
+                found = tree_find(root->right, data);
+
+        return found;
+}
 
 void visit_tree(node *root, void (*action)(node *nd))
 {
